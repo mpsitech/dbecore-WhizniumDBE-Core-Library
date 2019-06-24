62b4c24b71b164960e4649c0b4fc6c18bb432b27
Alexander Wirthmueller <aw@mpsitech.com>
2019-06-24 14:23:21 +0200

    relaunch with generalized paths

---
+++ _rls/dbecore_linux/checkout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# file checkout.sh
+# checkout script for Whiznium DBE core library, release dbecore_linux
+# author Alexander Wirthmueller
+# date created: 8 Feb 2016
+# modified: 24 Jun 2019
+
+export set SRCROOT=${WHIZROOT}/src
+
+mkdir ${WHIZROOT}/dbecore
+
+cp Makefile ${WHIZROOT}/dbecore/
+
+cp ../../*.h ${WHIZROOT}/dbecore/
+cp ../../*.cpp ${WHIZROOT}/dbecore/
